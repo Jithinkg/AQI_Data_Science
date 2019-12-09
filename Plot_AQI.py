e6c0a82f90651651b0cd7a314d45ae8370a68e4c
Jithinkg <jithink7321@gmial.com>
2019-12-10 00:58:01 +0530

    added comments
    2nd commit

--- Plot_AQI.py
+++ Plot_AQI.py
@@ -152,11 +152,12 @@ if __name__=="__main__":
     plt.plot(range(0,365),lst2013,label="2013 data")
     plt.plot(range(0,364),lst2014,label="2014 data")
     plt.plot(range(0,365),lst2015,label="2015 data")
-    plt.plot(range(0,121),lst2016,label="2016 data")
+    plt.plot(range(0,365),lst2016,label="2016 data")
     plt.xlabel('Day')
     plt.ylabel('PM 2.5')
     plt.legend(loc='upper right')
     plt.show()
+	
 
 
 
